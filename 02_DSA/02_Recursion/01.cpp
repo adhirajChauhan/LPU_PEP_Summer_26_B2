785e2d8d3973ca89dda42c09678b1f13475b1f22
adhirajChauhan <adhirajsingh0105@gmail.com>
2026-07-01 09:14:07 +0530

    01072026

---
+++ 02_DSA/02_Recursion/01.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    if(n ==0 || n ==1){
+        return 1;
+    }
+    return n * factorial(n-1);
+}
+ 
+int fibbonacci(int n){
+    if(n ==0) return 0;
+    if(n == 1) return 1;
+
+    return fibbonacci(n-1) + fibbonacci(n-2);
+}
+int main()
+{
+ 
+}
---
+++ 02_DSA/03_2D_Arrays/01.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+void printEvenColumn(int arr[][], int row, int col){
+
+}
+ 
+int main()
+{
+    // int arr[3][3] = {1,2,3,4,5,6,7,8,9};
+    // int arr[3][3] = {{1,2,3},
+    //                  {4,5,6},
+    //                  {7,8,9}
+    //                 };
+
+    int arr[3][4];
+    //user input row wise
+    // for(int i = 0; i < 3; i++){
+    //     for(int j = 0; j < 4; j++){
+    //         cin >> arr[i][j];
+    //     }
+    // }
+    //user input column wise
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 3; j++){
+            cin >> arr[j][i];
+        }
+    }
+ 
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
