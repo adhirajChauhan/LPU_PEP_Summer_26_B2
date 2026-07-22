95a1fc64c40f27e5e38f06c936d429f8dfa2d770
adhirajChauhan <adhirajsingh0105@gmail.com>
2026-07-22 09:03:02 +0530

    22072026

---
+++ 09_Graphs/02.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<list>
+using namespace std;
+ 
+int main()
+{
+    int n; // number of vertices
+    int m; //number of edges
+
+    cout << "Enter the number of vertices : ";
+    cin >> n;
+    cout << "Enter the number of edges : ";
+    cin >> m;
+
+    unordered_map<int, list<int>> adjList;
+    for(int i = 0; i < m; i++){
+        int u, v;
+        cin >> u >> v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u); //write this if graph is undirected
+
+    }
+    cout << "Adjacency list : " << endl;
+
+    for(auto i : adjList){
+        cout << i.first << " -> ";
+        for(auto j : i.second){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+ 
+}
